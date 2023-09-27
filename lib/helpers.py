7af9f8e4f59762f964dc65c0428949b65366a247
Jmillamena <jcmillamena@gmail.com>
2023-09-27 09:41:36 -1000

    edits filter function

--- lib/helpers.py
+++ lib/helpers.py
@@ -153,4 +153,11 @@ def delete_activity():
 
 def filter_activities_by_trip_id():
     try:
-        
+        trip_id = int(input("\033[34mEnter trip ID to filter activities: \033[0m"))
+        activities = Activity.filter_by_trip_id(trip_id)
+        if activities:
+            for activity in activities:
+                print(activity)
+        else:
+            print(f'\033[31mNo activities found for trip ID {trip_id}\033[0m')
+    except:
